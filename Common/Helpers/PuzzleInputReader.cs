5a095bd2234213ecc17306ba355af4779bd4690e
Jeb Bishop <jeb@jeb.zone>
2022-12-04 16:08:08 -0500

    Add project files.

---
+++ Common/Helpers/PuzzleInputReader.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Helpers
+{
+    public class PuzzleInputReader
+    {
+        public static async Task<IEnumerable<string>> GetPuzzleInputs()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+            string[] lines = await File.ReadAllLinesAsync(path.ToString());
+
+            return lines.ToList();
+        }
+    }
+}
---
+++ Day01/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Common.Helpers;
+
+IEnumerable<string> lines = await PuzzleInputReader.GetPuzzleInputs();
+
+List<int> elfTotals = new();
+
+int calorieAccumulator = 0;
+foreach (string line in lines)
+{
+    if (line == "")
+    {
+        elfTotals.Add(calorieAccumulator);
+        calorieAccumulator = 0;
+    }
+    else
+    {
+        calorieAccumulator += int.Parse(line);
+    }
+}
+
+Console.WriteLine($"Max: {elfTotals.Max()}");
+Console.WriteLine($"Sum of 3 highest: {elfTotals.OrderByDescending(c => c).Take(3).Sum()}");
