e9465ba61b06fc23c6f1afad7cbd71be3b899eba
Jeb Bishop <jeb@jeb.zone>
2022-12-04 17:38:04 -0500

    Day 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Common.Helpers;
+﻿using Common.Helpers;
 
 IEnumerable<string> lines = await PuzzleInputReader.GetPuzzleInputs();
 
---
+++ Day02/RPSResolver.cs
@@ -0,0 +1,114 @@
+﻿using System.Diagnostics;
+
+namespace Day02
+{
+    public class RPSResolver
+    {
+        private const int LossValue = 0;
+        private const int TieValue = 3;
+        private const int WinValue = 6;
+
+        private const char RockOpponent = 'A';
+        private const char PaperOpponent = 'B';
+        private const char ScissorsOpponent = 'C';
+
+        private const char RockPlayer = 'X';
+        private const char PaperPlayer = 'Y';
+        private const char ScissorsPlayer = 'Z';
+
+        private const char LossOutcome = 'X';
+        private const char TieOutcome = 'Y';
+        private const char WinOutcome = 'Z';
+
+        private static readonly Dictionary<char, int> shapeValues = new()
+        {
+            { RockPlayer, 1 },
+            { PaperPlayer, 2 },
+            { ScissorsPlayer, 3 },
+        };
+
+        public static int ResolveRound(char opponentMove, char playerMove)
+        {
+            if (MovesAreEquivalent(playerMove, opponentMove)) return GetPointValue(TieValue, playerMove);
+
+            switch (playerMove)
+            {
+                case RockPlayer:
+                    if (opponentMove == ScissorsOpponent) return GetPointValue(WinValue, playerMove);
+                    break;
+                case PaperPlayer:
+                    if (opponentMove == RockOpponent) return GetPointValue(WinValue, playerMove);
+                    break;
+                case ScissorsPlayer:
+                    if (opponentMove == PaperOpponent) return GetPointValue(WinValue, playerMove);
+                    break;
+            }
+
+            return GetPointValue(LossValue, playerMove);
+        }
+
+        public static int ResolveWithCheating(char opponentMove, char neededOutcome)
+        {
+            char playerMove;
+            if (neededOutcome == TieOutcome)
+            {
+                playerMove = GetPlayerEquivalent(opponentMove);
+            }
+            else if (neededOutcome == LossOutcome)
+            {
+                switch (opponentMove)
+                {
+                    case RockOpponent:
+                        playerMove = ScissorsPlayer;
+                        break;
+                    case PaperOpponent:
+                        playerMove = RockPlayer;
+                        break;
+                    case ScissorsOpponent:
+                        playerMove = PaperPlayer;
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+            else if (neededOutcome == WinOutcome)
+            {
+                switch (opponentMove)
+                {
+                    case RockOpponent:
+                        playerMove = PaperPlayer;
+                        break;
+                    case PaperOpponent:
+                        playerMove = ScissorsPlayer;
+                        break;
+                    case ScissorsOpponent:
+                        playerMove = RockPlayer;
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+            else throw new ArgumentException();
+
+            return ResolveRound(opponentMove, playerMove);
+        }
+
+        private static bool MovesAreEquivalent(char moveA, char moveB)
+        {
+            return GetPlayerEquivalent(moveB) == moveA;
+        }
+
+        private static char GetPlayerEquivalent(char moveA)
+        {
+            if (moveA == RockOpponent) return RockPlayer;
+            if (moveA == PaperOpponent) return PaperPlayer;
+            if (moveA == ScissorsOpponent) return ScissorsPlayer;
+            throw new ArgumentException();
+        }
+
+        private static int GetPointValue(int pointsFromOutcome, char shape)
+        {
+            return pointsFromOutcome + shapeValues[shape];
+        }
+    }
+}
