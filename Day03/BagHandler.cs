34eb8ec9629c4e77635bdcbaeb885139f317df8c
Jeb Bishop <jeb@jeb.zone>
2022-12-04 22:02:18 -0500

    Day 3

---
+++ Day03/BagHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Day03
+{
+    internal class BagHandler
+    {
+
+        public static int ProcessBag(string bag)
+        {
+            char commonItem = FindCommonItem(bag);
+
+            return PriorityHelper.GetItemPriority(commonItem);
+        }
+
+        private static char FindCommonItem(string bag)
+        {
+            string compartment1 = bag.Substring(0, bag.Length / 2);
+            string compartment2 = bag.Substring(bag.Length / 2);
+
+            var commonChar = compartment1.Where(c => compartment2.Contains(c)).First();
+
+            return commonChar;
+        }
+
+        public static int FindCommonItemPriority(string bag1, string bag2, string bag3)
+        {
+            char commonChar = bag1.Where(c => bag2.Contains(c) && bag3.Contains(c)).First();
+
+            return PriorityHelper.GetItemPriority(commonChar);
+        }
+    }
+}
---
+++ Day03/PriorityHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace Day03
+{
+    internal class PriorityHelper
+    {
+        private const int AsciiLowerStart = 96;
+        private const int AsciiUpperStart = 64;
+        private const int LowerPriorityStart = 0;
+        private const int UpperPriorityStart = 26;
+
+        public static int GetItemPriority(char item)
+        {
+            int asciiValue = (int)item;
+
+            if (asciiValue > AsciiLowerStart)
+            {
+                return GetLowerCaseValue(item);
+            }
+            else
+            {
+                return GetUpperCaseValue(item);
+            }
+        }
+
+        private static int GetLowerCaseValue(char item)
+        {
+            return item - AsciiLowerStart + LowerPriorityStart;
+        }
+
+        private static int GetUpperCaseValue(char item)
+        {
+            return item - AsciiUpperStart + UpperPriorityStart;
+        }
+    }
+}
