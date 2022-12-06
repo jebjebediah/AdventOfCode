d909847c9e8f9d355868385e7e29d61b550484a7
Jeb Bishop <jeb@jeb.zone>
2022-12-05 22:51:11 -0500

    Day 5 Part 1

---
+++ Day05/CargoHold.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Day05
+{
+    internal class CargoHold
+    {
+        private List<Stack<char>> cargoStacks;
+
+        public CargoHold(int width)
+        {
+            cargoStacks = new();
+
+            for (int i = 0; i < width; i++)
+            {
+                cargoStacks.Add(new Stack<char>());
+            }
+        }
+
+        public void AddToStack(int index, char c)
+        {
+            cargoStacks.ElementAt(index).Push(c);
+        }
+
+        private char RemoveFromStack(int index)
+        {
+            return cargoStacks.ElementAt(index).Pop();
+        }
+
+        public void DoMove(int fromIndex, int toIndex, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                char movingCargo = RemoveFromStack(fromIndex);
+                AddToStack(toIndex, movingCargo);
+            }
+        }
+
+        public string GetTop()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (Stack<char> stack in cargoStacks)
+            {
+                sb.Append(stack.Peek());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
