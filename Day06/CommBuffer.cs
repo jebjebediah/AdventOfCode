d0b2562106d17069e64aaa285c7ba9894f946a79
Jeb Bishop <jeb@jeb.zone>
2022-12-06 19:31:59 -0500

    Day 6 Part 1

---
+++ Day06/CommBuffer.cs
@@ -0,0 +1,37 @@
+﻿namespace Day06
+{
+    internal class CommBuffer
+    {
+        private Queue<char> buffer;
+        private int bufferSize;
+
+        public CommBuffer(int bufferSize)
+        {
+            buffer = new Queue<char>();
+            this.bufferSize = bufferSize;
+        }
+
+        public void AddToBuffer(char c)
+        {
+            buffer.Enqueue(c);
+            if (buffer.Count > bufferSize)
+            {
+                buffer.Dequeue();
+            }
+        }
+
+        public bool CheckForPacket()
+        {
+            if (buffer.Count == bufferSize)
+            {
+                IEnumerable<char> characters = buffer.Distinct();
+                if (characters.Count() == bufferSize)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
