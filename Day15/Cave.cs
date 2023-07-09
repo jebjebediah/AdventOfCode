ed457205271fc68e26226a3059dc5c9c92e81491
Jeb Bishop <jeb@jeb.zone>
2023-07-09 12:34:51 -0400

    First WIP

--- Day15/Cave.cs
+++ Day15/Cave.cs
@@ -2,20 +2,81 @@ namespace Day15
 {
     public class Cave
     {
-        public AddSensor(int xPos, int yPos, int xBeacon, int yBeacon)
-        {
+        private List<Sensor> Sensors;
 
+        public Cave()
+        {
+            Sensors = new List<Sensor>();
+        }
+
+        public void AddSensor(int xPos, int yPos, int xBeacon, int yBeacon)
+        {
+            Sensors.Add(new Sensor(xPos, yPos, xBeacon, yBeacon));
+        }
+
+        public int FilledAtYRow(int yRow)
+        {
+            int filledCount = 0;
+
+            foreach (Sensor sensor in Sensors)
+            {
+                filledCount += sensor.OccupiedAtYRow(yRow);
+            }
+
+            return filledCount;
         }
     }
 
-    private struct CavePosition
+    internal struct CavePosition
     {
-        int X;
-        int Y;
+        internal int X;
+        internal int Y;
+    }
 
-        internal int DistanceTo(CavePosition other)
+    internal struct Sensor
+    {
+        internal CavePosition Position;
+        internal CavePosition NearestBeaconPosition;
+        internal int RadiusToBeacon;
+        
+        internal Sensor(int xPos, int yPos, int xBeacon, int yBeacon)
         {
-            return int.Abs(this.X - end.X) + int.Abs(this.Y - end.Y);
+            Position = new CavePosition
+            {
+                X = xPos,
+                Y = yPos
+            };
+
+            NearestBeaconPosition = new CavePosition
+            {
+                X = xBeacon,
+                Y = yBeacon
+            };
+
+            RadiusToBeacon = int.Abs(NearestBeaconPosition.X - Position.X) +
+                             int.Abs(NearestBeaconPosition.Y - Position.Y);
+        }
+
+        internal bool RadiusContainsYRow(int yRow)
+        {
+            return Position.Y + RadiusToBeacon >= yRow || Position.Y - RadiusToBeacon <= yRow;
+        }
+
+        internal int OccupiedAtYRow(int yRow)
+        {
+            // TODO: Double-counting
+            int distanceFromRow = int.Abs(Position.Y - yRow);
+
+            int maxRadius = (2 * RadiusToBeacon) + 1;
+
+            int radiusAtRow = maxRadius - (2 * distanceFromRow);
+
+            if (NearestBeaconPosition.Y == yRow)
+            {
+                radiusAtRow--;
+            }
+
+            return radiusAtRow >= 0 ? radiusAtRow : 0;
         }
     }
 }
--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,19 +1,22 @@
 ﻿using Common.Helpers;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, 15!");
+using Day15;
+using System.Text.RegularExpressions;
 
 Cave cave = new Cave();
 
 IEnumerable<string> lines = await PuzzleInputReader.GetPuzzleInputs("sample.txt");
-Regex r = new Regex(@"(\d+)");
+Regex r = new Regex(@"(-?\d+)");
 
 foreach (string line in lines)
 {
     MatchCollection matches = r.Matches(line);
 
-    cave.AddSensor(int.Parse(matches[0]),
-                   int.Parse(matches[1]),
-                   int.Parse(matches[2]),
-                   int.Parse(matches[3]));
+    Console.WriteLine($"{matches.ElementAt(0).Value},{matches.ElementAt(1).Value},{matches.ElementAt(2).Value},{matches.ElementAt(3).Value}");
+
+    cave.AddSensor(int.Parse(matches.ElementAt(0).Value),
+                   int.Parse(matches.ElementAt(1).Value),
+                   int.Parse(matches.ElementAt(2).Value),
+                   int.Parse(matches.ElementAt(3).Value));
 }
+
+Console.WriteLine(cave.FilledAtYRow(10));
