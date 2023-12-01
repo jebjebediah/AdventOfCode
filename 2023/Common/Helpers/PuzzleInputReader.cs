52d658d51b9ea098ca2b60cc2b1d321fb63c0e67
Jeb Bishop <jeb@jeb.zone>
2023-12-01 00:29:35 -0500

    Day 01 Part 1

---
+++ 2023/Common/Helpers/PuzzleInputReader.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.Helpers;
+
+public class PuzzleInputReader
+{
+    public static async Task<IEnumerable<string>> GetPuzzleInputs(string fileName = "input.txt")
+    {
+        var path = Path.Combine(Environment.CurrentDirectory, fileName);
+
+        string[] lines = await File.ReadAllLinesAsync(path.ToString());
+
+        return lines.ToList();
+    }
+}
