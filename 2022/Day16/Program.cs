0769039fc69a265923086cd03f650bad01dab47d
Jeb Bishop <jeb@jeb.zone>
2023-11-30 20:39:56 -0500

    Move 2022 to a folder

---
+++ 2022/Day16/Program.cs
@@ -0,0 +1,20 @@
+﻿using Common.Helpers;
+using System.Text.RegularExpressions;
+
+IEnumerable<string> lines = await PuzzleInputReader.GetPuzzleInputs("sample.txt");
+Regex r = new Regex(@"Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnels? leads? to valves? ((?:[A-Z][A-Z](?:, )?)+)");
+
+foreach (string line in lines)
+{
+    MatchCollection matches = r.Matches(line);
+
+    var match = matches.ElementAt(0);
+    var groups = match.Groups;
+
+    string valveName = groups[1].Value;
+    int rate = int.Parse(groups[2].Value);
+
+    string connectionNames = groups[3].Value;
+
+    string[] splitNames = connectionNames.Replace(" ", "").Split(",");
+}
