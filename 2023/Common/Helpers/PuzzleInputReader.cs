ab6a499373ad4a1fc317cb988431ef46bfe75423
Jeb Bishop <jeb@jeb.zone>
2023-12-02 00:47:43 -0500

    Scaffold for easier setup in the future

--- 2023/Common/Helpers/PuzzleInputReader.cs
+++ 2023/Common/Helpers/PuzzleInputReader.cs
@@ -2,11 +2,34 @@
 
 public class PuzzleInputReader
 {
-    public static async Task<IEnumerable<string>> GetPuzzleInputs(string fileName = "input.txt")
+    public static async Task<IEnumerable<string>> GetPuzzleInputs(string problemNumber, int part, bool isSample)
     {
-        var path = Path.Combine(Environment.CurrentDirectory, fileName);
+        string basePath = Path.Combine(Environment.CurrentDirectory, "Inputs");
 
-        string[] lines = await File.ReadAllLinesAsync(path.ToString());
+        string extension = isSample ? "sample" : "input";
+        
+        string finalPath;
+        if (isSample)
+        {
+            string firstAttemptFileName = $"{problemNumber}.{part}.{extension}";
+            var path = Path.Combine(basePath, firstAttemptFileName);
+            if (File.Exists(path))
+            {
+                finalPath = path;
+            }
+            else
+            {
+                string secondAttemptFileName = $"{problemNumber}.{extension}";
+                finalPath = Path.Combine(basePath, secondAttemptFileName);
+            }
+        }
+        else
+        {
+            string fileName = $"{problemNumber}.{extension}";
+            finalPath = Path.Combine(basePath, fileName);
+        }
+
+        string[] lines = await File.ReadAllLinesAsync(finalPath);
 
         return lines.ToList();
     }
---
+++ 2023/Problems/Day01/Main.cs
@@ -0,0 +1,43 @@
+﻿using Common.Helpers;
+using Common;
+
+namespace Problems.Day01;
+
+public class Main : IAdventOfCodeProblem
+{
+    public string ProblemNumber => "01";
+
+    public async Task<string> RunPart1(bool useSample)
+    {
+        IEnumerable<string> lines = await PuzzleInputReader.GetPuzzleInputs(ProblemNumber, 1, useSample);
+
+        List<int> twoDigitNums = new();
+
+        foreach (string line in lines)
+        {
+            List<int> digits = new List<int>();
+
+            foreach (char c in line)
+            {
+                if (char.IsDigit(c))
+                {
+                    digits.Add((int)char.GetNumericValue(c));
+                }
+            }
+
+            int numA = digits.First();
+            int numB = digits.Last();
+
+            int newNum = (numA * 10) + numB;
+
+            twoDigitNums.Add(newNum);
+        }
+
+        return twoDigitNums.Sum().ToString();
+    }
+
+    public Task<string> RunPart2(bool useSample)
+    {
+        throw new NotImplementedException();
+    }
+}
