0c530d31118398bd0dfeea7d2b2170f45ff196b0
Jeb Bishop <jeb@jeb.zone>
2023-12-09 07:27:19 -0500

    Test for part 2

--- 2023/Test/ProblemsTests.cs
+++ 2023/Test/ProblemsTests.cs
@@ -44,4 +44,10 @@ public class ProblemTests
     {
         await CommonTestPart1(new Problems.Day03.Main(), "4361");
     }
+
+    [TestMethod]
+    public async Task TestDay03Part2()
+    {
+        await CommonTestPart2(new Problems.Day03.Main(), "467835");
+    }
 }
